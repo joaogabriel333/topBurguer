2ef72a389d8e4bbae7257945252ea36b3c5dfbf1
joaogabriel333 <joao.gabriel.souzaiclououd.com@gmail.com>
2024-04-04 15:45:04 -0300

    atualizar

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProdutoControler;
+use Database\Seeders\ProdutoSeeder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/produtos', [ProdutoControler::class, 'index']);
+Route::post('/produtos', [ProdutoControler::class, 'store']);
